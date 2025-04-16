4930f0b82fb8e22c0010c550901e645bfa4630ab
bot-dd <botdd445@gmail.com>
2025-04-16 15:02:10 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,18 +8,18 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAEelXXHhNoWXR_duKpDJiFl39LbI7A3sU0")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7651012082:AAFVqWXSI3tBaq8igfQehzBNnP9hfCVwhPw")
+ADMIN = int(os.environ.get("ADMIN", '7822720438'))
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "MLTBRM")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "RM_MirrorLeech")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://UPLOADXPRO24BOT:UPLOADXPRO24BOT@cluster0.hjfk60f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 CAPTION = os.environ.get("CAPTION", "")
-group = environ.get('GROUP', '-1002173560131')
+group = environ.get('GROUP', '-1002311502682')
 GROUP = int(group) if group and id_pattern.search(group) else None
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+SUNRISES_PIC= ""  # Replace with your Telegraph link
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '7945551029'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8080"))
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002571138456)
