8a22a022921e751c62e4872bd1d8ffe04e155c87
bot-dd <botdd445@gmail.com>
2025-04-16 16:53:42 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = os.environ.get("API_ID", "10811400")
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+API_ID = os.environ.get("API_ID", "26649585")
+API_HASH = os.environ.get("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7651012082:AAFVqWXSI3tBaq8igfQehzBNnP9hfCVwhPw")
 ADMIN = int(os.environ.get("ADMIN", '7822720438'))
 FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "MLTBRM")
 FSUB_GROUP = os.environ.get("FSUB_GROUP", "RM_MirrorLeech")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://UPLOADXPRO24BOT:UPLOADXPRO24BOT@cluster0.hjfk60f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://rohanahamed75:gt4RXJZ1mUtOh4Xv@mmtg.0ong5.mongodb.net/?retryWrites=true&w=majority&appName=mmtg")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Rahat")
 CAPTION = os.environ.get("CAPTION", "")
 group = environ.get('GROUP', '-1002311502682')
 GROUP = int(group) if group and id_pattern.search(group) else None
